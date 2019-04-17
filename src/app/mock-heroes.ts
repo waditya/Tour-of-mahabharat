8af198be54c564467de6590f555b6918f9de3acc
waditya <waditya02@gmail.com>
2019-04-16 23:02:54 -0500

    Added hero-detail component

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    {id: 11, name: "Bheeshma"}, 
+    {id: 12, name: "Krishna"},
+    {id: 13, name: "Yudishthir"},
+    {id: 14, name: "Bheem"},
+    {id: 15, name: "Arjun"},
+    {id: 16, name: "Nakul"},
+    {id: 17, name: "Sahadev"},
+    {id: 18, name: "Duryodhan"},
+    {id: 19, name: "Karna"},
+    {id: 20, name: "Drona"},
+]
